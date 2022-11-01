b0576fa4ea733bd516c81dcdcb2306bbeaf93c52
GhaniAtmaja <malwo19@gmail.com>
2022-11-01 17:56:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('May 17, 2025 00:00:00').getTime();
+    const countDate = new Date('Nov 17, 2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
